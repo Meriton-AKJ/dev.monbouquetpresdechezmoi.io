608fcdcd3c9822adc9070bddaa501fc11438f7a0
Meriton-AKJ <meritonaskaj01@gmail.com>
2025-06-02 18:51:12 +0200

    commit 16 :  ajout du panier dans catalogue coté vieuw

--- app/view/public/catalogue/catalogue.php
+++ app/view/public/catalogue/catalogue.php
@@ -1,23 +1,27 @@
 <?php if (!empty($data['seasons']) && is_array($data['seasons'])): ?>
-    <?php foreach ($data['seasons'] as $season): ?>
-        <a href=""><?= htmlspecialchars($season['name']) ?></a><br>
-    <?php endforeach; ?>
+  <?php foreach ($data['seasons'] as $season): ?>
+    <a href=""><?= htmlspecialchars($season['name']) ?></a><br>
+  <?php endforeach; ?>
 <?php endif; ?>
 
-  <section class="catalogue-catalogue">
-    <?php
-    if (!empty($data['mesproduits']) && is_array($data['mesproduits'])) {
-      foreach ($data['mesproduits'] as $product) {
-        echo '<article class="card">';
-        echo '<hr>';
-        echo '<h2>' . $product['title'] . '</h2>';
-        echo '<img src="/assets/images/' . $product['avatar'] . '" alt="' . $product['title'] . '">';
-        echo '<p>' . $product['description'] . '</p>';
-        echo '<p>Prix: ' . $product['price'] . ' €</p>';
-        echo '<a href="/catalogue/item/' . $product['slug'] . '">Voir le produit</a>';
-        echo '</article>';
-      }
+<section class="catalogue-catalogue">
+  <?php
+  if (!empty($data['mesproduits']) && is_array($data['mesproduits'])) {
+    foreach ($data['mesproduits'] as $product) {
+      echo '<article class="card">';
+      echo '<hr>';
+      echo '<h2>' . $product['title'] . '</h2>';
+      echo '<img src="/assets/images/' . $product['avatar'] . '" alt="' . $product['title'] . '">';
+      echo '<p>' . $product['description'] . '</p>';
+      echo '<p>Prix: ' . $product['price'] . ' €</p>';
+      echo '<div class = "stock">';
+      echo '<a href="/catalogue/item/' . $product['slug'] . '">Voir le produit</a>';
+      echo '<a href="/panier" class="cart-icon">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </a>';
+      echo '</div>';
+      echo '</article>';
     }
-    ?>
-  </section>
-
+  }
+  ?>
+</section>
