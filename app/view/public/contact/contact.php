39fa84206d9df70dbc373fbab95d4c6be02d5577
Meriton-AKJ <meritonaskaj01@gmail.com>
2025-06-10 15:53:46 +0200

    commit 14 :  ajout de la partie infos

--- app/view/public/contact/contact.php
+++ app/view/public/contact/contact.php
@@ -5,14 +5,18 @@ if (isset($data['message'])) {
 
 ?>
 
-<form action="/contact/send" method="POST" id="contact-form">
-    <div class="form-group">
-        <label for="name">Nom</label>
-        <input type="text" name="name" class="form-control" placeholder="Entrer votre nom" value="<?= $data['post_data']["name"] ?? ''; ?>">
-    </div>
-    <div class="form-group">
-        <label for="prenom">prenom</label>
-        <input type="text" name="prenom" class="form-control" placeholder="Entrer votre prenom " value="<?= $data['post_data']["prenom"] ?? ''; ?>">
+<div class="contact-container">
+    <!-- Formulaire de contact -->
+    <form action="/contact/send" method="POST" id="contact-form">
+        <div class="form-group">
+            <label for="name">Nom</label>
+            <input type="text" name="name" class="form-control" placeholder="Entrer votre nom" value="<?= $data['post_data']["name"] ?? ''; ?>">
+        </div>
+
+        <div class="form-group">
+            <label for="prenom">Prénom</label>
+            <input type="text" name="prenom" class="form-control" placeholder="Entrer votre prénom" value="<?= $data['post_data']["prenom"] ?? ''; ?>">
+        </div>
 
         <div class="form-group">
             <label for="email">Email</label>
@@ -24,5 +28,20 @@ if (isset($data['message'])) {
             <textarea name="message" class="form-control" rows="4" placeholder="Entrer votre message"><?= $data['post_data']["message"] ?? ''; ?></textarea>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+
+    <!-- Informations de contact -->
+    <div class="contact-info">
+        <h3><mark>Informations de contact</mark></h3>
+        <p><strong>Adresse :</strong> 66 Groenveld, Zaventem 1930, Belgique</p>
+        <p><strong>Téléphone :</strong> +32 487 72 54 35</p>
+        <p><strong>Email :</strong> monbouquetpresdechezmoi@info.be</p>
+        <p><strong>Horaires :</strong></p>
+        - Lundi : 9h - 12h
+        <br>- Mardi - Vendredi : 9h - 17h
+        <br>- Samedi : 12h - 15h</p>
+
+
+    </div>
+</div>
